18f07d69cd095963b074f792deefc94a4b0a0893
Adam Bethlehem <adam.bethlehem@safetyio.com>
2026-06-10 07:56:30 +0200

    Completed user component: next is user list component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS, UserData } from './user-data';
 
 @Component({
@@ -9,4 +9,12 @@ import { DUMMY_USERS, UserData } from './user-data';
 })
 export class User {
   userList: UserData[] = DUMMY_USERS;
+  selectedUser = signal<UserData | null>(null);
+  imagePath = computed(() => `assests/users/${this.selectedUser()?.avatar}`);
+
+  onUserClick(user: UserData) {
+    this.selectedUser.set(user);
+    console.log(this.imagePath());
+    console.log(`${this.selectedUser()?.name} clicked!`);
+  }
 }
